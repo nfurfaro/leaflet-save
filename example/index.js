a3cb01df21a44fe2ef4fa69f02d84d2bd275c0c6
Nick Furfaro <nfurfaro33@gmail.com>
2017-06-08 15:58:26 -0600

    Cleanup

--- example/index.js
+++ example/index.js
@@ -21,8 +21,7 @@
 }(function leafletSaveFactory (L) {
   var SaveMapMixin = {
     save: function (mapState) {
-            // alert('It worked!')
-            // Build the state here
+      // Build the state here
       mapState = {
         'zoom': map.getZoom(),
         'center': map.getCenter(),
@@ -30,12 +29,6 @@
         'layers': []
       }
 
-      // map.eachLayer(function (layer) {
-      //   console.log(typeof (layer))
-      //   console.log(layer)
-      // })
-
-      state.layers = []
       map.eachLayer(function (layer) {
         if (layer instanceof L.TileLayer) {
           var l = {}
@@ -45,105 +38,12 @@
           mapState.layers.push(l)
         }
       })
-
       console.log(JSON.stringify(state.layers))
-      console.log(state.layers)
 
       // Print mapState to div here.
       document.getElementById('state').innerHTML = JSON.stringify(mapState, null, '\t')
     }
   }
 
-  // var SaveMapInitHook = function () {
-  //   this.on('layeradd', function (e) {
-  //           // If added layer is currently loading, spin !
-  //     if (e.layer.loading) this.spin(true)
-  //     if (typeof e.layer.on !== 'function') return
-  //     e.layer.on('data:loading', function () {
-  //       this.spin(true)
-  //     }, this)
-  //     e.layer.on('data:loaded', function () {
-  //       this.spin(false)
-  //     }, this)
-  //   }, this)
-  //   this.on('layerremove', function (e) {
-  //           // Clean-up
-  //     if (e.layer.loading) this.spin(false)
-  //     if (typeof e.layer.on !== 'function') return
-  //     e.layer.off('data:loaded')
-  //     e.layer.off('data:loading')
-  //   }, this)
-  // }
-
   L.Map.include(SaveMapMixin)
-  // L.Map.addInitHook(SaveMapInitHook)
 }, window))
-
-// (function (factory, window) {
-//     // define an AMD module that relies on 'leaflet'
-//   if (typeof define === 'function' && define.amd) {
-//     define(['leaflet'], function (L) {
-//       factory(L)
-//     })
-
-//     // define a Common JS module that relies on 'leaflet'
-//   } else if (typeof exports === 'object') {
-//     module.exports = function (L) {
-//       if (L === undefined) {
-//         L = require('leaflet')
-//       }
-//       factory(L)
-//       return L
-//     }
-//     // attach your plugin to the global 'L' variable
-//   } else if (typeof window !== 'undefined' && window.L) {
-//     factory(window.L)
-//   }
-// }(function leafletSaveFactory (L) {
-//   var SaveMapMixin = {
-//     save: function (state, options) {
-//       if (state) {
-//                 // start spinning !
-//         if (!this._spinner) {
-//           this._spinner = new Spinner(options)
-//                         .spin(this._container)
-//           this._spinning = 0
-//         }
-//         this._spinning++
-//       } else {
-//         this._spinning--
-//         if (this._spinning <= 0) {
-//                     // end spinning !
-//           if (this._spinner) {
-//             this._spinner.stop()
-//             this._spinner = null
-//           }
-//         }
-//       }
-//     }
-//   }
-// // Edit inithook function
-//   var SaveMapInitHook = function () {
-//     this.on('layeradd', function (e) {
-//             // If added layer is currently loading, spin !
-//       if (e.layer.loading) this.spin(true)
-//       if (typeof e.layer.on !== 'function') return
-//       e.layer.on('data:loading', function () {
-//         this.spin(true)
-//       }, this)
-//       e.layer.on('data:loaded', function () {
-//         this.spin(false)
-//       }, this)
-//     }, this)
-//     this.on('layerremove', function (e) {
-//             // Clean-up
-//       if (e.layer.loading) this.spin(false)
-//       if (typeof e.layer.on !== 'function') return
-//       e.layer.off('data:loaded')
-//       e.layer.off('data:loading')
-//     }, this)
-//   }
-
-//   L.Map.include(SaveMapMixin)
-//   L.Map.addInitHook(SaveMapInitHook)
-// }, window))
